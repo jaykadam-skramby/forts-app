658b7778fd3f199633573f86412e09775eeeab5a
jaykadam-skramby <jay.kadam@skramby.com>
2025-07-11 10:36:04 +0530

    Inital commit - deployed Node.js project

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
